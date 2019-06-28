2f8bc66fcbbe5efc5037be434bb90a093cf35437
Caroline Sarre <anacarolinesarre@gmail.com>
2019-06-28 19:18:58 -0300

    Adicionando novos campos e permitindo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_27_205947) do
+ActiveRecord::Schema.define(version: 2019_06_28_220218) do
 
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "product_id"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 2019_06_27_205947) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "phone"
+    t.boolean "isadmin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
