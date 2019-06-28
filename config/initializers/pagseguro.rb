45661f09b1af8591bb1b2d18b266e308a88f2d30
Caroline Sarre <anacarolinesarre@gmail.com>
2019-06-28 17:02:01 -0300

    Criando login com devise e fazendo integração com sandbox do pagseguro

---
+++ config/initializers/pagseguro.rb
@@ -0,0 +1,6 @@
+PagSeguro.configure do |config|
+    config.token       = "91D3365533CC44BE83CEEA4A1BD89FB5"
+    config.email       = "anacarolinesarre@gmail.com"
+    config.environment = :sandbox # ou :sandbox. O padrão é production.
+    config.encoding    = "UTF-8"
+  end
