0bf92bf7d7d0c2ef1d9771f18dc8c3efe7882230
Caroline Sarre <anacarolinesarre@gmail.com>
2019-06-29 06:54:18 -0300

    Virando pagseguro pra produção

--- config/initializers/pagseguro.rb
+++ config/initializers/pagseguro.rb
@@ -1,6 +1,6 @@
 PagSeguro.configure do |config|
-    config.token       = "91D3365533CC44BE83CEEA4A1BD89FB5"
+    config.token       = "f0ac81f1-ad87-4caa-8c28-191020b57094a72e241f499998b4ca77a291cf2a9ba9c7b0-f65a-438a-af76-9451ac95639e"
     config.email       = "anacarolinesarre@gmail.com"
-    config.environment = :sandbox # ou :sandbox. O padrão é production.
+    config.environment = :production # ou :sandbox. O padrão é production.
     config.encoding    = "UTF-8"
   end
